ae2ee782a54bcd2c1d3d2b5659ddc725f1a3533b
Mohammed Ragab <regoo707@hotmail.com>
2019-12-26 14:51:58 +0200

    Revert "Initial commit"
    This reverts commit 7f07b9f1fec2076456ffd76bbee5fc05917ce2e6.

--- back-end/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  var port = process.env.PORT
-  await app.listen(port);
-}
-bootstrap();
