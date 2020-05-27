6d5afce5921b9c435789130e257675f4a61f3035
Mohammed <regoo707@gmail.com>
2020-05-26 20:09:31 -0400

    remove app

--- app/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  var port = process.env.PORT || 3000
-  await app.listen(port);
-}
-bootstrap();
